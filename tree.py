e58eb4099fa1fce47fed9bf6a15300ea16c65747
Maxim <toper.one111@gmail.com>
2023-05-16 00:02:08 +0300

    Add inorder traversal and finding all possible sums

--- tree.py
+++ tree.py
@@ -10,18 +10,30 @@ class BST:
         self.root = None
 
     def build_from_array(self, arr):
-        sorted_arr = sorted([i for i in arr if i != 0])
-        self.root = self._build_tree(sorted_arr, 0, len(sorted_arr) - 1)
+        sorted_arr = sorted([i for i in arr if i != 0], reverse=True)
+        print(sorted_arr)
+        self.root = self.reconstruct_tree(arr)
+        self.inorder_traversal(self.root, sorted_arr)
 
-    def _build_tree(self, arr, start, end):
-        if start > end:
+    def reconstruct_tree(self, preorder):
+        if not preorder:
             return None
 
-        middle = (start + end) // 2
-        leftTree = self._build_tree(arr, start, middle - 1)
-        rightTree = self._build_tree(arr, middle + 1, end)
+        value = preorder.pop(0)
+        if value == 0:
+            return None
 
-        return Node(arr[middle], leftTree, rightTree)
+        node = Node(value)
+        node.left = self.reconstruct_tree(preorder)
+        node.right = self.reconstruct_tree(preorder)
+
+        return node
+
+    def inorder_traversal(self, node, sorted):
+        if node:
+            self.inorder_traversal(node.left, sorted)
+            node.value = sorted.pop()
+            self.inorder_traversal(node.right, sorted)
 
     def insert(self, value):
         if self.root is None:
@@ -52,3 +64,36 @@ class BST:
         if node.left is not None:
             self._pretty_print(node.left, prefix +
                                ("    " if is_left else "|   "), True)
+
+    def find_monotonic_paths(self, node, target_sum, current_path, current_sum, result, visited):
+        if node is None:
+            return
+
+        current_path.append(node.value)
+        current_sum += node.value
+
+        if current_sum == target_sum:
+            path_tuple = tuple(current_path)
+            if path_tuple not in visited:
+                result.append(current_path[:])
+                visited.add(path_tuple)
+
+        self.find_monotonic_paths(node.left, target_sum,
+                                  current_path, current_sum, result, visited)
+        self.find_monotonic_paths(node.right, target_sum,
+                                  current_path, current_sum, result, visited)
+
+        current_path.pop()
+        current_sum -= node.value
+
+        self.find_monotonic_paths(
+            node.left, target_sum, [], 0, result, visited)
+        self.find_monotonic_paths(
+            node.right, target_sum, [], 0, result, visited)
+
+    def find_all_monotonic_paths(self, target_sum):
+        result = []
+        visited = set()
+        self.find_monotonic_paths(
+            self.root, target_sum, [], 0, result, visited)
+        return result
