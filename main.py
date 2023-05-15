d636cb485408e2ba5ddeb4c7b6d3db6660de34a3
Maxim <toper.one111@gmail.com>
2023-05-15 19:16:03 +0300

    Create printing method

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from tree import BST
 
 input_file = sys.argv[1]
 sum_number = int(sys.argv[2])
@@ -8,3 +9,7 @@ with open(input_file) as f:
 
 print(A)
 print(sum_number)
+
+bst = BST()
+bst.build_from_array(A)
+bst.printTree()
--- tree.py
+++ tree.py
@@ -10,7 +10,8 @@ class BST:
         self.root = None
 
     def build_from_array(self, arr):
-        for value in arr:
+        sorted_arr = sorted(arr)
+        for value in sorted_arr:
             if value != 0:
                 self.insert(value)
 
@@ -31,3 +32,15 @@ class BST:
                 node.right = Node(value)
             else:
                 self._insert_recursive(node.right, value)
+
+    def printTree(self):
+        self._pretty_print(self.root, '', True)
+
+    def _pretty_print(self, node, prefix, is_left):
+        if node.right is not None:
+            self._pretty_print(node.right, prefix +
+                               ("|   " if is_left else "    "), False)
+        print(prefix + ("└── " if is_left else "┌── ") + str(node.value))
+        if node.left is not None:
+            self._pretty_print(node.left, prefix +
+                               ("    " if is_left else "|   "), True)
