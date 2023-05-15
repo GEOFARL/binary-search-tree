6b173d67eba8b5cb27fcdc3966fca6c5d5bc4eda
Maxim <toper.one111@gmail.com>
2023-05-15 18:49:28 +0300

    Implement recursive insert method

--- tree.py
+++ tree.py
@@ -11,10 +11,23 @@ class BST:
 
     def build_from_array(self, arr):
         for value in arr:
-            self.insert(value)
+            if value != 0:
+                self.insert(value)
 
     def insert(self, value):
         if self.root is None:
             self.root = Node(value)
         else:
             self._insert_recursive(self.root, value)
+
+    def _insert_recursive(self, node, value):
+        if value < node.value:
+            if node.left is None:
+                node.left = Node(value)
+            else:
+                self._insert_recursive(node.left, value)
+        else:
+            if node.right is None:
+                node.right = Node(value)
+            else:
+                self._insert_recursive(node.right, value)
