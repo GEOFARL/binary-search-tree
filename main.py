ec95c419c0f7f4e5dff16e715ce3fd70ad6313e3
Maxim <toper.one111@gmail.com>
2023-05-15 18:42:55 +0300

    Create a Node class

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import sys
 
 input_file = sys.argv[1]
-number = int(sys.argv[2])
+sum_number = int(sys.argv[2])
 
 with open(input_file) as f:
     A = [int(i) for i in f.readline().split()]
 
 print(A)
-print(number)
+print(sum_number)
