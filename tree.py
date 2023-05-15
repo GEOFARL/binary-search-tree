b909f45410bf604387c2e120936c61af7356a035
Maxim <toper.one111@gmail.com>
2023-05-15 18:46:16 +0300

    Create methods for building a tree and inserting

--- tree.py
+++ tree.py
@@ -8,3 +8,13 @@ class Node:
 class BST:
     def __init__(self):
         self.root = None
+
+    def build_from_array(self, arr):
+        for value in arr:
+            self.insert(value)
+
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self._insert_recursive(self.root, value)
